d6202cba2f308d22a04732adf3276824972acf44
himanshus0764 <152205558+himanshus0764@users.noreply.github.com>
2024-06-16 10:49:06 +0530

    Add files via upload

---
+++ leetcode 330.py
@@ -0,0 +1,12 @@
+# n=[1,2,4,5,10]
+# subarr=[]
+# total=0
+# for i  in range(len(n)):
+#     for j in range(i,len(n)):
+#         subarr.append(sum(n[i:j+1]))
+# subarr.sort()
+# print(subarr)
+total=0
+for i in range(1,20):
+    total+=i
+print(total)
